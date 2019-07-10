542e9ac3d7cb02335b21decfeb6e41be5f9966b4
Daniel Williams <daniellanierwilliams@gmail.com>
2019-07-10 18:45:16 -0500

    initial push, created files and installed express, mysql and handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Dave', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steve', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Regis', 0);
